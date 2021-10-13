bf4949d3b00cba82fa8dd86b9c3bd0dc38e14388
Muhammad Abbas <muhammdabbas.cs@gmail.com>
2021-10-13 11:13:40 +0500

    Added the Firebase, Auth, Firestore Pods into the project and Register the user on firebase

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        self.titleLabel.text = "⚡️FlashChat"
     }
     
 
